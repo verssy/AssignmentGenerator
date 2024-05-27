79f6ecfbbc0c7d0e50d806717e5f51b454df768b
verssy <verssy@mail.ru>
2024-05-27 23:07:08 +0700

    Bayes task supports several subtypes

--- Tasks/TaskBase.hpp
+++ Tasks/TaskBase.hpp
@@ -7,8 +7,8 @@ public:
     TaskBase(const std::wstring &taskName);
 
     virtual void Randomize() = 0;
-    virtual std::wstring GetDescription() = 0;
     virtual void Solve() = 0;
+    virtual std::wstring GetDescription() = 0;
     virtual std::wstring GetAnswer() = 0;
 
 protected:
--- Tasks/TaskBayes.cpp
+++ Tasks/TaskBayes.cpp
@@ -2,7 +2,7 @@
 #include "TaskBase.hpp"
 #include "TaskFactory.hpp"
 
-#include <format>
+#include "Format.hpp"
 
 class TaskBayes : public TaskBase {
 public:
@@ -12,27 +12,44 @@ public:
     void Randomize() override
     {
         for (int64_t i = 0; i < 3; i++) {
-            // [0.01; 0.99]
+            // [0.1; 0.9], step == 0.1
             probs[i] = (rand() % 9 + 1) / 10.0;
+        }
+
+        machinesCount = rand() % 3 + 1;
+    }
+
+    void Solve() override
+    {
+        switch (machinesCount) {
+            default:
+            case 1:
+                answer = probs[0] * probs[1] * (1 - probs[2]) +
+                         probs[0] * (1 - probs[1]) * probs[2] +
+                         (1 - probs[0]) * probs[1] * probs[2];
+                return;
+            case 2:
+                answer = probs[0] * (1 - probs[1]) * (1 - probs[2]) +
+                         (1 - probs[0]) * probs[1] * (1 - probs[2]) +
+                         (1 - probs[0]) * (1 - probs[1]) * probs[2];
+                return;
+            case 3:
+                answer = (1 - probs[0]) * (1 - probs[1]) * (1 - probs[2]);
+                return;
         }
     }
 
     std::wstring GetDescription() override
     {
-        return std::format(
-            L"Решите с использованием формул полной вероятности и Байеса (3 балла)\n"
-            "Рабочий обслуживает 3 станка.\n"
-            "Вероятность того, что в течение часа не потребует внимания рабочего:\n"
-            "первый станок равна {}, второй – {}, третий – {}.\n"
-            "Найти вероятность того, что в течение часа потребуют внимания 2 станка.",
-            probs[0], probs[1], probs[2]);
-    }
-
-    void Solve() override
-    {
-        answer = probs[0] * (1 - probs[1]) * (1 - probs[2]) +
-                 (1 - probs[0]) * probs[1] * (1 - probs[2]) +
-                 (1 - probs[0]) * (1 - probs[1]) * probs[2];
+        switch (machinesCount) {
+            default:
+            case 1:
+                return Format(descriptionOneMachine, probs[0], probs[1], probs[2]);
+            case 2:
+                return Format(descriptionTwoMachines, probs[0], probs[1], probs[2]);
+            case 3:
+                return Format(descriptionThreeMachines, probs[0], probs[1], probs[2]);
+        }
     }
 
     std::wstring GetAnswer() override
@@ -41,8 +58,30 @@ public:
     }
 
 private:
+    int machinesCount;
     double probs[3];
     double answer;
+
+    std::wstring descriptionOneMachine =
+        L"Решите с использованием формул полной вероятности и Байеса (3 балла)\n"
+        "Рабочий обслуживает 3 станка.\n"
+        "Вероятность того, что в течение часа не потребует внимания рабочего:\n"
+        "первый станок равна {}, второй – {}, третий – {}.\n"
+        "Найти вероятность того, что в течение часа потребует внимания 1 станок.";
+
+    std::wstring descriptionTwoMachines =
+        L"Решите с использованием формул полной вероятности и Байеса (3 балла)\n"
+        "Рабочий обслуживает 3 станка.\n"
+        "Вероятность того, что в течение часа не потребует внимания рабочего:\n"
+        "первый станок равна {}, второй – {}, третий – {}.\n"
+        "Найти вероятность того, что в течение часа потребуют внимания 2 станка.";
+
+    std::wstring descriptionThreeMachines =
+        L"Решите с использованием формул полной вероятности и Байеса (3 балла)\n"
+        "Рабочий обслуживает 3 станка.\n"
+        "Вероятность того, что в течение часа не потребует внимания рабочего:\n"
+        "первый станок равна {}, второй – {}, третий – {}.\n"
+        "Найти вероятность того, что в течение часа потребуют внимания 3 станка.";
 };
 
 Registrator<TaskBayes> registrator(L"Задача о подбрасывании монетки");
